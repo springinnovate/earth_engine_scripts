16f5275e33fcb7666253cfccd12c09b9a10090b1
Rich Sharp <richpsharp@gmail.com>
2021-10-11 12:18:02 -0700

    adding output field

--- ee_tracer.py
+++ ee_tracer.py
@@ -6,31 +6,39 @@ import pandas
 
 CSV_PATH = 'cotton_site_info.csv'
 
+DATASET = 'LANDSAT/LT05/C01/T1_8DAY_NDVI'
+START_DATE = '1997-01-01'
+END_DATE = '2019-01-01'
+
+HEADER_FIELD = f'{DATASET}_{START_DATE}_{END_DATE}'
+
 
 def main():
     """Entry point."""
     ee.Initialize()
     table = pandas.read_csv(CSV_PATH)
 
+    print(f'reading points from {CSV_PATH}')
     pts = ee.FeatureCollection([
         ee.Feature(
             ee.Geometry.Point(row['long'], row['lat']),
             row.to_dict())
-        for index, row in table.dropna().iterrows() if index == 1])
-    print(pts)
+        for index, row in table.dropna().iterrows()])
 
     img = ee.ImageCollection("LANDSAT/LT05/C01/T1_8DAY_NDVI").filterDate('1997-01-01', '2019-01-01')
     mean_img = img.reduce(ee.Reducer.mean())
     print('starting google earth engine sample')
+    REDUCER = 'first'
     samples = mean_img.reduceRegions(**{
         'collection': pts,
         'scale': 30,
-        'reducer': 'first'}).getInfo()
+        'reducer': REDUCER}).getInfo()
     with open(f'sampled_{os.path.basename(CSV_PATH)}', 'w') as table_file:
-        table_file.write(','.join(table.columns) + '\n')
+        table_file.write(','.join(table.columns) + f',{HEADER_FIELD}\n')
         for sample in samples['features']:
             table_file.write(','.join([
-                str(sample['properties'][key]) for key in table.columns])+'\n')
+                str(sample['properties'][key]) for key in table.columns]) +
+                f",{sample['properties'][REDUCER]}\n")
 
 
 if __name__ == '__main__':
