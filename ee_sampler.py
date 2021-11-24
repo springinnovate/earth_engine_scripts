6d8c18b8345fd52fd5ab64ed214b032b1895ba2b
Rich Sharp <richpsharp@gmail.com>
2021-11-24 09:05:46 -0800

    adding proportion of cover for natural types

--- ee_sampler.py
+++ ee_sampler.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import os
 
 import ee
+import numpy
 import pandas
 
 CSV_PATH = 'data_table_citrusformatics_MKL.csv'
@@ -10,6 +11,36 @@ YEAR_FIELD = 'crop_year'
 LONG_FIELD = 'field_longitude'
 LAT_FIELD = 'field_latitude'
 REDUCER = 'mean'
+BUFFER = 300
+NLCD_DATASET = 'USGS/NLCD_RELEASES/2016_REL'
+NLCD_VALID_YEARS = numpy.array([1992, 2001, 2004, 2006, 2008, 2011, 2013, 2016])
+
+
+def _get_closest_num(number_list, candidate):
+    """Return closest number in sorted list."""
+    index = numpy.searchsorted(number_list, candidate)
+    if index == len(number_list):
+        index -= 1
+    closest_candidate = str(number_list[index])
+    return closest_candidate
+
+
+def _nlcd_natural_cultivated_mask(year):
+    """Natural for NLCD in 41-74 or 90-95."""
+    closest_year = _get_closest_num(NLCD_VALID_YEARS, year)
+    nlcd_dataset = ee.ImageCollection(NLCD_DATASET)
+    nlcd_year = nlcd_dataset.filter(
+        ee.Filter.eq('system:index', closest_year)).first().select('landcover')
+    # natural 41-74 & 90-95
+    natural_mask = ee.Image(0).where(
+        nlcd_year.gte(41).And(nlcd_year.lte(74)).Or(
+            nlcd_year.gte(90).And(nlcd_year.lte(95))), 1)
+    natural_mask = natural_mask.rename(f'NLCD-natural')
+
+    cultivated_mask = ee.Image(0).where(
+        nlcd_year.gte(81).And(nlcd_year.lte(82)), 1)
+    cultivated_mask = natural_mask.rename(f'NLCD-cultivated')
+    return natural_mask, cultivated_mask, closest_year
 
 
 def _sample_pheno(pts_by_year):
@@ -44,12 +75,18 @@ def _sample_pheno(pts_by_year):
     header_fields_with_prev_year = [
         x for field in header_fields
         for x in (field, field+'-prev-year')]
+    header_fields_with_prev_year.append(f'NLCD-natural')
+    header_fields_with_prev_year.append(f'NLCD-cultivated')
 
     sample_list = []
     for year in pts_by_year.keys():
         print(f'processing year {year}')
         year_points = pts_by_year[year]
         all_bands = None
+
+        nlcd_natural_mask, nlcd_cultivated_mask, closest_year = \
+            _nlcd_natural_cultivated_mask(year)
+
         for active_year, band_name_suffix in (
                 (year, ''), (year-1, '-prev-year')):
             current_year = datetime.strptime(
@@ -77,11 +114,23 @@ def _sample_pheno(pts_by_year):
             raw_variable_bands = raw_variable_bands.rename(raw_band_names)
             all_bands = all_bands.addBands(raw_variable_bands)
 
+            # mask raw variable bands by cultivated
+            #ImageCollection.iterate(algorithm, first)
+            cultivated_variable_bands = ee.Image([None]*len(raw_band_names)).where(
+                nlcd_cultivated_mask.eq(1), raw_variable_bands)
+            all_bands = all_bands.add(cultivated_variable_bands)
+
+            # mask raw variable bands by natural
+
+        print('append bands')
+        all_bands = all_bands.addBands(nlcd_natural_mask)
+        all_bands = all_bands.addBands(nlcd_cultivated_mask)
+        print('reduce regions')
         samples = all_bands.reduceRegions(**{
             'collection': year_points,
-            'scale': 2000,
             'reducer': REDUCER}).getInfo()
         sample_list.extend(samples['features'])
+    print(sample_list[0])
     return header_fields_with_prev_year, sample_list
 
 
@@ -96,7 +145,7 @@ def main():
     for year in table[YEAR_FIELD].unique():
         pts_by_year[year] = ee.FeatureCollection([
             ee.Feature(
-                ee.Geometry.Point(row[LONG_FIELD], row[LAT_FIELD]),
+                ee.Geometry.Point(row[LONG_FIELD], row[LAT_FIELD]).buffer(BUFFER),
                 row.to_dict())
             for index, row in table[
                 table[YEAR_FIELD] == year].dropna().iterrows()])
@@ -105,7 +154,7 @@ def main():
     header_fields, sample_list = _sample_pheno(pts_by_year)
     print(header_fields)
     print(len(sample_list[0]))
-    with open(f'sampled_{os.path.basename(CSV_PATH)}', 'w') as table_file:
+    with open(f'sampled_{BUFFER}m_{os.path.basename(CSV_PATH)}', 'w') as table_file:
         table_file.write(
             ','.join(table.columns) + f',{",".join(header_fields)}\n')
         for sample in sample_list:
